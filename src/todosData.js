ddc597a71c64a07bd3c9c7f8ce46c14da1132e78
Luci Temple <lucitemple@yahoo.com.au>
2021-10-13 13:33:50 +1100

    Initial commit

---
+++ src/todosData.js
@@ -0,0 +1,34 @@
+const todosData = [
+  {
+    id: 1,
+    text: "Take out the trash",
+    completed: true,
+  },
+  {
+    id: 2,
+    text: "Grocery shopping",
+    completed: false,
+  },
+  {
+    id: 3,
+    text: "Clean fish tank",
+    completed: false,
+  },
+  {
+    id: 4,
+    text: "Mow the lawn",
+    completed: false,
+  },
+  {
+    id: 5,
+    text: "Learn React",
+    completed: true,
+  },
+  {
+    id: 6,
+    text: "Take Rest",
+    completed: false,
+  },
+];
+
+export default todosData;
