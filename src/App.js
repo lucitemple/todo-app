7b8ab34a2135d6a0d3e5131bf912dc6bf54dd649
Luci Temple <lucitemple@yahoo.com.au>
2021-10-13 14:17:57 +1100

    Add todo items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import ToDoItem from "./ToDoItem";
+import todosData from "./todosData";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { todos: todosData };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(id) {
+    this.setState((prevState) => {
+      //alert((JSON.stringify(prevState.todos[0].id)));
+      const updatedTodos = prevState.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      });
+      return {
+        todos: updatedTodos,
+      };
+    });
+  }
+
   render() {
-    return (
-      <div>
-        Hello World
-      </div>
-    )
+    const todoItems = this.state.todos.map((item) => (
+      <ToDoItem item={item} key={item.id} handleChange={this.handleChange}/>
+    ));
+    return <div>{todoItems}</div>;
   }
 }
-
--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
 
-function ToDoItem() {
-    return (
-        <div>
-            
-        </div>
-    )
+function ToDoItem(props) {
+  return (
+    <div>
+      <input
+        type="checkbox"
+        checked={props.item.completed}
+        onChange={() => props.handleChange(props.item.id)}
+      />
+      <p>{props.item.text}</p>
+    </div>
+  );
 }
 
-export default ToDoItem
-
-
+export default ToDoItem;
